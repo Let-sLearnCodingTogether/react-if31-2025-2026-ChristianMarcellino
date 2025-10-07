a961ee2edd0718a35c720dc6164c5608822861c7
Christian Marcellino <christianmarcellino14@gmail.com>
2025-10-07 14:36:49 +0700

    Wawan Ohim

--- src/pages/public/authentication/login/Login.jsx
+++ src/pages/public/authentication/login/Login.jsx
@@ -3,7 +3,7 @@ import Logo from "../../../../assets/components/logo/Logo";
 export default function Login(){
     return <div className="grid min-h-screen grid-cols-1 md:grid-cols-2">
     <div className="hidden bg-image p-5 md:block">
-        <div className="flex flex-col justify-between">
+        <div className="flex h-full flex-col justify-between">
             <div className="flex items-center space-x-3">
                 <Logo />
                 <div className="font-semibold text-2xl text-white tracking-tight">
